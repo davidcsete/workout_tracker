874bf83b8c6ca889da7bf115f9862390186d79fb
David Csete Fogel <david@192.168.0.105>
2025-07-16 18:24:25 +0300

    Added Macro chart

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,26 @@
 class HomeController < ApplicationController
+  before_action :authenticate_user!
   def index
+    today_macros
+  end
+
+  private
+
+  def today_macros
+    meals_today = current_user.meals.where(created_at: Time.zone.today.all_day)
+
+    totals = meals_today.joins(:food_items).select(
+      "SUM(food_items.calories) as total_calories",
+      "SUM(food_items.protein) as total_protein",
+      "SUM(food_items.carbs) as total_carbs",
+      "SUM(food_items.fats) as total_fats"
+    ).take
+
+    @total_calories = totals.total_calories.to_i
+    @macros = {
+      protein: totals.total_protein.to_f,
+      carbs: totals.total_carbs.to_f,
+      fats: totals.total_fats.to_f
+    }
   end
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,12 +6,14 @@ import "@rails/ujs"
 // Rails.start() // <-- This initializes UJS to handle method: :delete etc.
 import { application } from "controllers/application"
 import StepsController from "controllers/steps_controller"
-import WorkoutChartController from "controllers/chart_controller"
+import WorkoutChartController from "controllers/workout_chart_controller"
 import FoodSearchController from "controllers/food_search_controller"
+// import MacroChartController from "controllers/macro_chart_controller"
 // import FlashController from "./controllers/flash_controller"
 application.register("chart", WorkoutChartController)
 application.register("steps", StepsController)
 application.register("autocomplete", FoodSearchController)
 // application.register("flash", FlashController)
+// application.register("macro-chart", MacroChartController)
 
 Turbo.session.drive = true
